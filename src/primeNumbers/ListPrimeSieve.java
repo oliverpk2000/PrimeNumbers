c510a0cdcf9f301f64cf21a3d1dc652a88869b46
oliverpk2000 <oliver.pecek@gmail.com>
2022-11-09 20:21:30 +0100

    test

--- src/primeNumbers/ListPrimeSieve.java
+++ src/primeNumbers/ListPrimeSieve.java
@@ -1,10 +1,22 @@
 package primeNumbers;
 
+import java.util.ArrayList;
+
 /**
  * Created: 09/11/2022
  *
  * @author Oliver Pecek (oliver)
  */
 public class ListPrimeSieve {
-
+  public static int amountOfPrimes(int last){
+    ArrayList<Integer> knownPrimes = new ArrayList<>();
+    knownPrimes.add(1);
+    knownPrimes.add(2);
+    for (int i = 3; i <= last; i++) {
+      knownPrimes.add(i);
+    }
+    System.out.println("log");
+    System.out.println("kek");
+    return 0;
+  }
 }
